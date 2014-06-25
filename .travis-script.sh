0c2f016c4acedef9f8a2d42bebeaf81d7d2445d2
philopon <philopon.dependence@gmail.com>
2014-06-25 23:25:48 +0900

    travis.

---
+++ .travis-script.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash -eu
+
+printHeader () {
+  echo "################################################################################"
+  echo "                                   " $1
+  echo "################################################################################"
+}
+
+printHeader sql-simple
+cabal configure --enable-tests
+cabal test
+cabal install
+
+for path in `cat submodules`; do
+  cd $path
+  printHeader $path
+  cabal configure --enable-tests
+  cabal test
+  cabal install
+  cd ..
+done
+
